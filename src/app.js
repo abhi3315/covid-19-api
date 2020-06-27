f4b4cb77fdc3217b21c5829de5613dc9e29f6ef5
Abhishek Sharma <abhi243461@hotmail.com>
2020-06-27 22:42:33 +0530

    feat: Enable resource sharing with another origin

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const api = require('./utils/api')
 
 const port = process.env.PORT || 3000
 
 const app = express()
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, '../public')))
 
